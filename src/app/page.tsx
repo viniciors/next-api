adeb51c2958ed3c780168be2af5c3b50c2f40365
Vinicio Raphael Santana <vinicioraphael2012@gmail.com>
2024-04-18 09:40:44 -0300

    nem sei o que eu to fazendo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main> content </main>
+interface Item {
+  name: string;
+  address: string;
+}
+
+export default async function Home() {
+const response = await fetch('http://localhost:3000/api/clients');
+  
+const json = await response.json()
+
+console.log(json)
+
+
+return(
+  <main>
+      {
+        json.map((item: Item) => {
+          return (
+            <>
+              <div className="border bg-gray-300 my-3 p-2 flex justify-center"> 
+                <div className="uppercase flex-1">{item.name}</div>
+                <div className="flex-1 italic">{item.address}</div>
+              </div>
+            </>
+          )
+        })
+      }
+  </main>
   );
 }
